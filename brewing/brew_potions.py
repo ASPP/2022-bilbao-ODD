7331cc8d00020c631c46db04fde81cac90a54b3c
ASPP Student <student@localhost>
2022-09-07 11:51:12 +0200

    workflow exercise

--- brewing/brew_potions.py
+++ brewing/brew_potions.py
@@ -1,7 +1,8 @@
-import potion_class
-import containers
-import cooking
-import inspection
+import brewing.potion_class as potion_class
+import brewing.containers as containers
+import brewing.cooking as cooking
+import brewing.inspection as inspection
+import brewing.ingredients as ingredients
 
 
 def make_example_potion(student_name):
@@ -13,16 +14,29 @@ def make_example_potion(student_name):
     print("You have successfully run make_example_potion, well done :).")
     return my_potion
 
-
+"""
+1. Set up a pewter cauldron and light a fire underneath it
+2. Add fish eyes, unicorn hair and tea leaves
+3. Let simmer for 2 hours
+4. Have Snape inspect the potion (use target_potion='python_expert').
+"""
 def make_python_expert_potion(student_name):
-    print("I am a Python Expert")
-    # todo: write this function!
+    potion = potion_class.Potion(student_name=student_name)
+
+    #Set up a pewter cauldron and light a fire underneath it
+    potion.setup(container=containers.pewter_cauldron, heat_source=cooking.fire)
+
+    #Add fish eyes, unicorn hair and tea leaves
+    potion.add_ingredients(ingredients=[ingredients.fish_eyes, ingredients.unicorn_hair, ingredients.tea_leaves])
+
+    #Let simmer for 2 hours
+    cooking.simmer(potion, duration=2)
     
-    return
+    return potion
 
 
 if __name__ == "__main__":
-    my_name = 'ASPP student'
-    my_potion = make_example_potion(student_name=my_name)
+    my_name = 'GreatStudents'
+    my_potion = make_python_expert_potion(student_name=my_name)
     # Let Snape inspect the potion
-    inspection.inspection_by_Snape(potion=my_potion, target_potion='example_potion')
+    inspection.inspection_by_Snape(potion=my_potion, target_potion='python_expert')
--- brewing/example_usage_within_package.py
+++ brewing/example_usage_within_package.py
@@ -1,1 +1,5 @@
 # Task: import make_example_potion from the module brew_potions.py
+
+from brew_potions import make_example_potion
+
+make_example_potion(student_name="Tiwonge")
