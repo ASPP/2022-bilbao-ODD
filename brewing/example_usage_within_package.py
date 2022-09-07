c32dad49042344141ee4571654d2f32d46330a1c
ASPP Student <student@localhost>
2022-09-07 11:21:16 +0200

    Fix imports

--- brewing/example_usage_within_package.py
+++ brewing/example_usage_within_package.py
@@ -1,1 +1,4 @@
 # Task: import make_example_potion from the module brew_potions.py
+import brew_potions as br
+
+br.make_example_potion('HARRY')
--- example_usage_outside_package.py
+++ example_usage_outside_package.py
@@ -1,1 +1,4 @@
 # Task: import make_example_potion from the module brew_potions.py
+from brewing.brew_potions import make_example_potion
+
+make_example_potion('harry')
